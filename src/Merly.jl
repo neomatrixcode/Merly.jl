15252bcaa9b37109676f7dd9d59156959c729c70
neomatrix <josuecevedo@gmail.com>
2016-01-18 14:07:33 -0600

    diccionario para parametros y body

--- src/Merly.jl
+++ src/Merly.jl
@@ -7,7 +7,7 @@ export app, @route
 
 type Pag
 	method
-    route::Regex
+    route
     state
 end
 
@@ -15,44 +15,85 @@ type Fram
 	start::Function
 end
 
-function pag(url::Regex,res)
+function pag(url,res)
     Pag("Get",url,res)
 end
 
 NotFound = Response(404)
 
 b=[]
+params=Dict()
+body=Dict()
+
 
 macro route(exp1,exp2)
 	quote
-		push!(b,pag(Regex($exp1),$exp2))
+		push!(b,pag($exp1,$exp2))
 	end
 end
 
+function _url(ruta, resource)
+  ruta = split(ruta,"/")
+  ruta =ruta[2:length(ruta)]
+
+  resource = split(resource,"/")
+  resource =resource[2:length(resource)]
+  lruta=length(ruta)
+  lresource=length(resource)
+  s=true
+  if(lruta==lresource)
+    for i=1:lruta
+      if length(ruta[i])>=1
+        if !(ruta[i][1]==':')
+          println("ruta:  ",Regex(ruta[i]))
+          println("entrada:  ",resource[i])
+          println((ismatch(Regex(ruta[i]),resource[i])))
+          s=s && (ismatch(Regex(ruta[i]),resource[i]))
+        else
+          r=ruta[i][2:length(ruta[i])]
+          params[r]=resource[i]
+          println(params)
+        end
+      end
+    end
+    return s
+  end
+  return false
+end
 
 function handler(b,req,res)
+bo=""
 println("datos del request:")
 println(req.resource)
 println(req.method)
 println(req.headers)
 println(req.headers["Content-Type"])
-println(req.data)
-println("interpetando los Bytes de req.data como caracteres: ")
- for i=1:length(req.data)
-       print(Char(req.data[i]))
-       end
-println("\n--")
+if(length(req.data)>=1)
+  println("interpetando los Bytes de req.data como caracteres: ")
+  for i=1:length(req.data)
+    bo*="$(Char(req.data[i]))"
+  end
+  bo= replace(bo,Regex("{|}"),"",2)
+  println(bo)
+  db=split(bo,":")
+  body[db[1]]=db[2]
+  println(body)
+  println("\n--")
+end
+
 tam= length(b)
 
 if tam>0
 h = HttpCommon.headers()
 for s=1:tam
-	if ismatch(b[s].route,req.resource)
+	if _url(b[s].route,req.resource)
 		h["Content-Type"]="text/plain"
-		#res = Response()#OPCION 2
-		#res.status = 200
-		#res.body=b[s].state # manipulas cada parametro
-		return Response(200,h,b[s].state)
+    global params
+    res.headers=h
+		res.status = 200
+		res.data=b[s].state # manipulas cada parametro
+    params=Dict()
+		return res#Response(200,h,b[s].state)
 	end
 end
 end
@@ -74,34 +115,9 @@ end
 return Fram(start)
 end
 
-function _url(ruta, resource)
-  ruta = split(ruta,"/")
-  ruta =ruta[2:length(ruta)]
 
-  resource = split(resource,"/")
-  resource =resource[2:length(resource)]
-  lruta=length(ruta)
-  lresource=length(resource)
-  s=true
-  if(lruta==lresource)
-    for i=1:lruta
-      if length(ruta[i])>=1
-        if !(ruta[i][1]==':')
-          println("ruta:  ",Regex(ruta[i]))
-          println("entrada:  ",resource[i])
-          println((ismatch(Regex(ruta[i]),resource[i])))
-          s=s && (ismatch(Regex(ruta[i]),resource[i]))
-        else
-          println("$(ruta[i]) => $(resource[i])")
-        end
-      end
-    end
-    return s
-  end
-  return false
-end
 
-_url("/home/:hola/","/home/AZR/")
+#_url("/home/[A-Z]{1,3}/","/home/AZR/")
 end # module
 
 
