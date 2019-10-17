b97e5cf81544113aa5d99c41f3d39eb6f103c9c4
neomatrix <josuecevedo@gmail.com>
2019-10-17 00:10:05 -0500

    update test

--- src/base.jl
+++ src/base.jl
@@ -116,7 +116,7 @@ struct app
     function WebServer(ruta::String,exten::String)
       cd(rootbase*"/"*ruta)
       ls= readdir()
-      @info("ls",ls)
+      @info("pwd",pwd())
       for i=1:length(ls)
         cd(rootbase*"/"*ruta)
         if isfile(ls[i])
