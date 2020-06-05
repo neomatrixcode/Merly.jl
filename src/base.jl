6e310ee15d6308057cc3921cc9b2cfc2f4bf2c09
Phelipe Wesley <phelipewesleydeoliveira@gmail.com>
2020-06-05 16:59:54 -0300

    Update base.jl
    ident

--- src/base.jl
+++ src/base.jl
@@ -99,7 +99,7 @@ struct app
         extension="text/plain"
         ext= split(file,".")
         if(length(ext)>1)
-        my_extension = ext[end]
+          my_extension = ext[end]
           if (haskey(mimetypes,my_extension))
             extension=mimetypes[my_extension]
           end
